a63b99d93fc4a550d188d630b7c6ae12e56bb402
tmorita630 <92248709+tmorita630@users.noreply.github.com>
2021-10-24 19:14:39 +0900

    Update script.js

--- prg/jQuery/1/script.js
+++ prg/jQuery/1/script.js
@@ -5,3 +5,16 @@ $(function() {
   } );
   
 });
+
+$(function() {
+  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
+  $('#language-wrapper').hover(
+    function() {
+    $('.language-text').fadeIn();
+  },
+    function() {
+    $('.language-text').fadeOut();  
+  }
+  );
+  
+});
