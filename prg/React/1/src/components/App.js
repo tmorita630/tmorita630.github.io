c8ee291579521b021bf091cc4142155d8e764aea
tmorita630 <92248709+tmorita630@users.noreply.github.com>
2021-10-31 17:21:08 +0900

    Update App.js

--- prg/React/1/src/components/App.js
+++ prg/React/1/src/components/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello React</h1>
+    	<div>
+    	  <h1>こんにちは、にんじゃわんこさん！</h1>
+    	  {/* <button>タグ「ひつじ仙人」を追加してください */}
+    	  <button>ひつじ仙人</button>
+    	  
+    	  {/* <button>タグ「にんじゃわんこ」を追加してください */}
+    	  <button>にんじゃわんこ</button>
+    	  
+    	</div>
     );
   }
 }
